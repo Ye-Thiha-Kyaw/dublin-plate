aa649858c4ca7da0cabacfff2b1d0f5941827b00
Ntnick-22 <nyeinthunaing322@gmail.com>
2025-04-17 00:16:40 +0100

    footer link fixed,folder created,favicon added

---
+++ js/script2.js
@@ -0,0 +1,33 @@
+// Bootstrap Form Validation from the contact-us.html
+(() => {
+  "use strict";
+
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+setTimeout(() => {
+  document.getElementById("placeholder-card").style.display = "none";
+  document.getElementById("real-card").style.display = "block";
+}, 2000);
+const toastTrigger = document.getElementById("liveToastBtn");
+const toastLiveExample = document.getElementById("liveToast");
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
