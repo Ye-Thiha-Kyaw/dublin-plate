db3889a7dc1756ea485fe9eaf9867d9c4c7c11d4
tinmoelwin23 <fminntml@gmail.com>
2025-04-16 17:25:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-const collegeAddress = {
-  name: "National College of Ireland",
-  street: "Mayor Street Lower",
-  area: "IFSC",
-  city: "Dublin",
-  postcode: "D01 K6W2",
-  country: "Ireland",
-};
+
 // JavaScript tinmmoelwin
 
 document.addEventListener('DOMContentLoaded', function() {
